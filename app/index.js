ea051e45e146292af9f32cc752c4aca4783eb2ae
Jordan Dalton <jordan.8474@gmail.com>
2016-09-29 21:56:47 +0100

    Dammed you Azure!!
    Azure doesn't let you run clusters (or IISNode) so, I've had to remove it from production. I can add it back when deploying to own server.

--- app/index.js
+++ app/index.js
@@ -1,28 +1,34 @@
 
 var cluster = require("cluster");
+var myApp = require("./app.js"),
+    server = require('http').Server(myApp);
 
-if(cluster.isMaster){
+if (myApp.get("env") == "development"){
+    if(cluster.isMaster){
 
-    var cpuCount = require('os').cpus().length;
+        var cpuCount = require('os').cpus().length;
 
-    console.log("Setting up clustering for " + cpuCount + " CPUs");
+        console.log("Setting up clustering for " + cpuCount + " CPUs");
 
-    for(var i = 0; i < cpuCount; i++){
-        cluster.fork();
+        for(var i = 0; i < cpuCount; i++){
+            cluster.fork();
+        }
+
+        cluster.on("exit", function(worker){
+            console.log("Worker %d died :( I'm starting him up again.", worker.id);
+            cluster.fork();
+        });
+
+    }else{
+        // Start listening on the specified port
+        server.listen(myApp.get("port"), function(){
+        	console.log("Cluster %d listening on port: %d", cluster.worker.id, myApp.get("port") );
+        });
+
     }
-
-    cluster.on("exit", function(worker){
-        console.log("Worker %d died :( I'm starting him up again.", worker.id);
-        cluster.fork();
-    });
-
 }else{
-    var myApp = require("./app.js"),
-        server = require('http').Server(myApp);
-    
-    // Start listening on the specified port
+    console.log("Just using one CPU");
     server.listen(myApp.get("port"), function(){
-    	console.log("Cluster %d listening on port: %d", cluster.worker.id, myApp.get("port") );
+        console.log("Cluster %d listening on port: %d", cluster.worker.id, myApp.get("port") );
     });
-
 }
