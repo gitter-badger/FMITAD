b61fd5179fe6af941467db3a1efc47efb778fba2
Jordan Dalton <jordan.8474@gmail.com>
2016-10-15 13:05:12 +0100

    Session IDs in socketHelper
    A better way of handling the socket connections is to use the session ID of the user since the session ID is unique to the user and everytime we call " io() " on the client, we generate a new sokcet id.

--- app/lib/socketHelper.js
+++ app/lib/socketHelper.js
@@ -1,5 +1,7 @@
-var analytics = require("./sockets/analytics.js");
-var ua = require("express-useragent");
+var analytics = require("./sockets/analytics.js"),
+    ua = require("express-useragent"),
+    //mongoStore = require("connect-mongo"),
+    cookieParser = require("cookie-parser");
 
 var clients = {};
 
@@ -9,24 +11,62 @@ function Helper ( io ){
     }
 
     io.on("connection", function(socket){
-        console.log("User has connected: " + socket.id);
 
-        //clients.push(socket);
-        clients[socket.id] = { socket: socket };
-        analytics.initUser( clients[socket.id], ua.parse(socket.handshake.headers["user-agent"]) );
+        if (!socket.request.headers.cookie){
+            //They might not have a session yet.
+            return;
+        }
 
-        socket.on("page view", function( data ){
-            analytics.pageView(clients[socket.id], data.ref, data.loc);
-        });
+        var sessionId = getCookie(socket.request.headers.cookie, "connect.sid");
+        //console.log("User socket connected Sid:\n\t" + sessionId);
+        if (sessionId){
+            //Incase
+            if (clients[sessionId] && clients[sessionId].socket){
+                clients[sessionId].socket.disconnect();
+            }
 
-        socket.on("disconnect", function(){
-            delete clients[socket.id];
-        });
+            clients[sessionId] = { socket: socket };
+            analytics.initUser( clients[sessionId], ua.parse(socket.handshake.headers["user-agent"]) );
+
+            socket.on("page view", function( data ){
+                analytics.pageView(clients[sessionId], data.ref, data.loc);
+            });
+
+            socket.on("disconnect", function(){
+                delete clients[sessionId];
+            });
+
+        }
+
     });
 };
 
-Helper.sendNotification = function( socketId, notfication ){
-    clients[socketId].socket.emit("notification", notification);
+function getCookie(cookieString, cname) {
+    var name = cname + "=";
+    var ca = cookieString.split(';');
+    for(var i = 0; i <ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0)==' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length,c.length);
+        }
+    }
+    return "";
+}
+
+
+Helper.sendNotification = function( sessionId, notfication ){
+    clients[sessionId].socket.emit("notification", notification);
 };
+
+Helper.getSocketFor = function( sessionId ){
+    if (sessionId in clients){
+        return clients[sessionId].socket;
+    }
+
+    return undefined;
+}
 
 module.exports = Helper;
